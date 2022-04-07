798c51b5ca228da4c45b9ef60d8ff309520e9bca
Jacob <66694749+Jacob7532@users.noreply.github.com>
2022-04-07 14:32:17 -0700

    Update swampCooler.ino

--- swampCooler.ino
+++ swampCooler.ino
@@ -1,9 +1,16 @@
-void setup() {
+#include <LiquidCrystal.h>
+#include <Stepper.h>
+#include <time.h>
+#include <clock.h>
 
+
+volatile unsigned char* 
+
+void setup() {
+Serial.begin(9600)
 
 }
 
 void loop() {
-
 
 }
