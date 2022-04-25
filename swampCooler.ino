28ff87057e1d1ff6811d0c4e7d0779072bf40613
Jacob <66694749+Jacob7532@users.noreply.github.com>
2022-04-25 12:30:57 -0700

    Update swampCooler.ino

--- swampCooler.ino
+++ swampCooler.ino
@@ -1,16 +1,11 @@
+#include <DHT.h>
 #include <LiquidCrystal.h>
-#include <Stepper.h>
-#include <time.h>
-#include <clock.h>
+#include <RTClib.h>
 
+typedef enum{Off, Error, Idle, Running}
 
-volatile unsigned char* 
-
-void setup() {
-Serial.begin(9600)
-
-}
-
-void loop() {
-
-}
+//memory for ADC
+volatile uint8_t* const MUX = (uint8_t*) 0x7c
+volatile uint8_t* const SRA = (uint8_t*) 0x7a
+volatile uint8_t* const SRB = (uint8_t*) 0x7b
+volatile uint16_t* const Data = (uint16_t*) 0x78
