629782bdee95449ce2b3b9172c84929a575a9d09
Jacob <66694749+Jacob7532@users.noreply.github.com>
2022-04-29 11:53:17 -0700

    Update swampCooler.ino

--- swampCooler.ino
+++ swampCooler.ino
@@ -1,7 +1,8 @@
 #include <DHT.h>
 #include <LiquidCrystal.h>
+#include <RTClib.h>
 
-typedef enum{Off, Error, Idle, Running}
+typedef enum{Off, Error, Idle, Running} states;
 
 //memory for ADC
 volatile uint8_t* const MUX = (uint8_t*) 0x7c;
@@ -10,15 +11,16 @@ volatile uint8_t* const SRB = (uint8_t*) 0x7b;
 volatile uint16_t* const Data = (uint16_t*) 0x78;
 
 //macros
-#define DHTPIN xx
+#define DHTPIN 14
 #define DHTTYPE DHT11
-#define SENSORPIN xx
+#define SENSORPIN 0
 #define WATER 100
 #define TEMP 25
 
 
 //class objects
-DHT dht(DHTPIN, DHTTYPE);'LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 RTC_DS1307 rtc;
 
 
@@ -43,25 +45,34 @@ void adcDisable();
 void setup(){
 
   //lights
-  DDRH |= (1 << PH5); //yellow
-  DDRH |= (1 << PH6); //blue
-  DDRB |= (1 << PB4); //red
-  DDRB |= (1 << PB5); //green
+  DDRB |= (1 << PB4); //yellow
+  DDRH |= (1 << PH5); //blue
+  DDRB |= (1 << PB5); //red
+  DDRH |= (1 << PH6); //green
 
-  //button
-  DDRB &= ~(1 << PB6);
+  //buttonOn
+  DDRB &= ~(1 << PB7);
+
+  //buttonVent
+  DDRH &= ~(1 << PH4);
   
-  //motor
-  DDRA |= (1 << PA1);
-  DDRA |= (1 << PA3);
-  DDRA |= (1 << PA5);
+  //fan
+  DDRG |= (1 << PG5);
+  DDRE |= (1 << PE5);
+  DDRE |= (1 << PE4);
+
+  //vent
+  /*DDR |= ();
+  DDR |= ();
+  DDR |= ();
+  DDR |= ();*/
 
   //clock
   rtc.begin();
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
   //LCD
-  lcd.begin(16, 2)
+  lcd.begin(16, 2);
 
   //cooler
   coolerOffState();
@@ -92,61 +103,61 @@ void statesMachine(){
     switch(coolerState){
       
       case(Off):
-        //button pressed to turn on
-        if(button() == 0){
+        //buttonOn pressed to turn on
+        if(buttonOn() == 0){
           coolerIdleState();
         }
         break;
 
       case(Idle):
         //temp and water check
-        temperatureMonitor();
+        temperature();
         humidity();
         displayLCD();
         if(t >= TEMP){
-          RTCFcn();
+          printRTC();
           coolerRunningState();
         }
         if(*Data < WATER){
           coolerErrorState();
         }
-        if(button() == 0){
-          //button pressed to turn off
+        if(buttonOn() == 0){
+          //buttonOn pressed to turn off
           coolerOffState();
         }
         break;
 
        case(Running):
        //check temp and water
-        temperatureMonitor();
+        temperature();
         humidity();
         displayLCD();
         if(t < TEMP){
-          printRCT();
+          printRTC();
           coolerIdleState();
         }
         if(*Data < WATER){
-          printRCT();
+          printRTC();
           coolerErrorState();
         }
-        if(button() == 0){
-          //button pressed to print time.
-          RTCF0n();
+        if(buttonOn() == 0){
+          //buttonOn pressed to print time.
+          printRTC();
           //also turns off
           coolerOffState();
         }
         break;
 
        case(Error):
-        temperatureMonitor();
+        temperature();
         humidity();
         displayLCD();
         if(*Data >= WATER){
           coolerIdleState();
         }
-        if(button() == 0){
-          //button press to turn off
-          coolerDisableState();
+        if(buttonOn() == 0){
+          //buttonOn press to turn off
+          coolerOffState();
         }
         break;
     }
@@ -196,42 +207,127 @@ void coolerIdleState(){
 
 void coolerRunningState(){
   
+  driveLow();
+  coolerState = Running;
+  //blue on
+  PORTH = 0b01000000;
+  //motor on
+  PORTA = 0b00001010;
+  //display to the lcd
+  displayLCD();
+  
+}
+
+
+void temperature(){
+  
+  adcDisable();
+  float previous = t;
+  t = dht.readTemperature();
+  if(isnan(t)){
+    t = previous;
+  }
+  adcInitialize();
+  
 }
 
 
 void humidity(){
   
+  adcDisable();
+  float previous = h;
+  h = dht.readHumidity();
+  if(isnan(h)){
+    h = previous;
+  }
+  adcInitialize();
+  
 }
 
 
-int button(){
+int buttonOn(){
+
+  adcDisable();
+  //debouncing
+  if((PINB & (1 << PB6))!=0){
+    _delay_ms(250);
+  }
+  if((PINB & (1 << PB6))!=0){
+    return 0;
+  }
+  else{
+    return 1;
+  }
+  adcInitialize();
   
 }
 
 
 void displayLCD(){
+  adcDisable();
+  if(coolerState == Off){
+    return;
+  }
+  else{
+    if(coolerState == Error){
+      lcd.setCursor(0,0);
+      lcd.print("Error");
+    }
+    lcd.setCursor(0,1);
+    lcd.print(t);
+    lcd.print("C ");
+    lcd.print(h);
+    lcd.print("%");
+  }
+  adcInitialize();
   
 }
 
 
-void printRCT(){
+void printRTC(){
+
+  DateTime now = rtc.now();
+  Serial.print(now.hour(), DEC);
+  Serial.print(":");
+  Serial.print(now.minute(), DEC);
+  Serial.print(":");
+  Serial.print(now.second(), DEC);
+  Serial.println();
   
 }
 
 
 void adcInitialize(){
+
+  //bits 0,1,2,3,4 for pin selection
+  //bits 6,7 for refrence selection
+  *MUX = 0b01000000;
+  //bit 0,1,2 prescaling
+  //bit 3 interrupt
+  //bit 4 signals conversion finish
+  //bit 5 contols autotriggers
+  //bit 6 conversion initialization
+  //bit 7 enable adc
+  *SRA = 0b10001000;
+  //bit 0,1,2 control autotrigger
+  //bit 6 comperator control
+  *SRB = 0b00000000;
+  //choose input pin
+  *MUX |= SENSORPIN;
+  //conversion initialization
+  *SRA |= (1 << 6);
   
 }
 
 
 void adcDisable(){
-  
+  //bit 7 to 0 to disable
 }
 
 
 void driveLow(){
-  PORTB = 0b00000000
-  PORTH = 0b00000000
+  PORTB = 0b00000000;
+  PORTH = 0b00000000;
 }
 
 
@@ -239,7 +335,8 @@ void driveLow(){
 
 
 ISR(ADC_vect){
-  
+
+  //interrupts and restarts conversion
   *SRA |= (1 << 6);
   
 }
